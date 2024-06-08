de77e44488470f6f009f177d18809868c3a34516
vansh-frontend <vanshdhalor04>
2024-06-08 20:30:10 +0530

    initial

---
+++ sort.js
@@ -0,0 +1,5 @@
+const a = [22, 12, 22, 34, 90, 22];
+
+a.sort((a, b) => a - b);
+
+console.log(a);
---
+++ with.js
@@ -0,0 +1,7 @@
+const lang = ["html", "css", "java"];
+
+const mylang = lang.with(2, "javascript");
+
+console.log("before update: ", lang);
+
+console.log("after update: ", mylang);
